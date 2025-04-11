a775f3a9342ab819a9b49d6959ffc222eac80fc4
illuv403 <s31388@pjwstk.edu.pl>
2025-04-12 01:00:19 +0200

    Models created

--- DB/database.py
+++ DB/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 """
 If executing on windows please set the URL of the database like that:
 engine = create_engine("sqlite:///C:\\path\\to\\foo.DB")
-A base class to initialise our database and create 
+A base class to initialise our database and create
 Base from which all models will inherit.
 """
 engine = create_engine('sqlite:////database.db', echo=True)
