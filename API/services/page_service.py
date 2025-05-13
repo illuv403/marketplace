289fc7d93b1e2c527fab0063d2e2bac64afe027e
prokh <prokorend@gmail.com>
2025-05-13 20:27:09 +0200

    updated page_service.py and run.py files placement

---
+++ API/services/page_service.py
@@ -0,0 +1,18 @@
+
+class PageService:
+    def __init__(self):
+        self.categories_list = []
+        self.user_cart = []
+        self.product_list = []
+
+    def get_random_products(self):
+        pass
+
+    def get_user_cart(self):
+        pass
+
+    def get_categories(self):
+        pass
+
+    def search(self):
+        pass
