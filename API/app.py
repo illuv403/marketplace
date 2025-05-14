be89025303be3949259c672f22d466c58cf74e67
illuv403 <s31388@pjwstk.edu.pl>
2025-05-14 21:15:20 +0200

    Auth logic now works

--- API/app.py
+++ API/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
+from API.services.auth_service import AuthService
 from API.services.main_page_service import PageService
 from DB.database import db_session, init_db
 from DB.fill_db import FillProducts
@@ -7,6 +8,7 @@ from DB.fill_db import FillProducts
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
+    app.secret_key = 'gnlrjjnarvnioernviloergvrvsd;vlckmasl;kdv'
 
     init_db()
     product_fill = FillProducts(db_session)
@@ -18,9 +20,41 @@ def create_app():
         products = page_service.get_random_products()
         return render_template('index.html' , products=products)
 
-    @app.route('/auth', methods=['GET'])
+    @app.route('/auth', methods=['GET', 'POST'])
     def auth():
+        auth_service = AuthService(session=db_session)
+
+        if request.method == 'POST':
+            action = request.form.get('action', 'login')
+
+            if action == 'register':
+                user = auth_service.register_user(
+                    name=request.form['name'],
+                    surname=request.form['surname'],
+                    login=request.form['login'],
+                    email=request.form['email'],
+                    password=request.form['password']
+                )
+
+                if user:
+                    auth_service.new_session(user)
+                    return redirect(url_for('index'))
+            elif action == 'login':
+                email = request.form['email']
+                password = request.form['password']
+
+                user = auth_service.login_user(email, password)
+
+                if user:
+                    auth_service.new_session(user)
+                    return redirect(url_for('index'))
+
         return render_template('author.html')
+
+    @app.route('/logout', methods=['GET'])
+    def logout():
+        AuthService.end_session()
+        return redirect(url_for('auth'))
 
     @app.route('/product')
     def product():
